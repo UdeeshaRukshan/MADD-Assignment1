7f801c2224bd279206e4a50b7eaeef616fc82a12
UdeeshaRukshan <IT21807480@my.sliit.lk>
2025-03-03 18:55:28 +0530

    Initial Commit

---
+++ Assignment1/Assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1App.swift
+//  Assignment1
+//
+//  Created by udeesha rukshan on 2025-03-03.
+//
+
+import SwiftUI
+
+@main
+struct Assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment1Tests/Assignment1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1Tests.swift
+//  Assignment1Tests
+//
+//  Created by udeesha rukshan on 2025-03-03.
+//
+
+import Testing
+@testable import Assignment1
+
+struct Assignment1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
